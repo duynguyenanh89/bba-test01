4b76a5e7d21ac56104db9adc15805e81a9226b05
test 01 - Duy Nguyen <duynguyenanh89@gmail.com>
2026-03-13 13:23:04 +0700

    init project

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,14 +1,15 @@
 import { test, expect } from '@playwright/test';
+const homepage = "https://e-commerce-dev.betterbytesvn.com";
 
 test('has title', async ({ page }) => {
-  await page.goto('https://e-commerce-dev.betterbytesvn.com/');
+  await page.goto(homepage);
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/E-commerce site for automation testing/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://e-commerce-dev.betterbytesvn.com/');
+  await page.goto(homepage);
 
   // Click the get started link.
   await page.getByRole('link', { name: 'My account' }).click();
