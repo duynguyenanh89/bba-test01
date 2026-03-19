f709438d0d2c83c314ac4a5408ed234869491b54
test 01 - Duy Nguyen <duynguyenanh89@gmail.com>
2026-03-19 17:06:24 +0700

    init project

---
+++ tests/example.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+const homepage = "https://e-commerce-dev.betterbytesvn.com";
+
+test('has title', async ({ page }) => {
+  await page.goto(homepage);
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/E-commerce site for automation testing/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto(homepage); 
+
+  // Click the My account link.
+  await page.getByRole('link', { name: 'My account' }).click();
+
+  // Expects page to have a heading with the name "My account".
+  await expect(page.getByRole('heading', { name: 'My account' })).toBeVisible();
+});
